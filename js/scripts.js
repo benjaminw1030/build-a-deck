a69744afa50fafd2391fdff72079204b00d5cca7
Ben Wilson <benjaminw1030@gmail.com>
2021-08-16 15:35:55 -0700

    add javascript to build deck with loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
-  $("#sentenceForm").submit(function (event) {
+  $("#deckForm").submit(function (event) {
     event.preventDefault();
-    const sentence = ($("#sentence").val()).split(' ');
-    const shortSentence = sentence.filter(word => word.length > 2);
-    console.log(shortSentence);
-    const reverseSentence = shortSentence.reverse();
-    console.log(reverseSentence);
-    $("#result").text(reverseSentence.join(' '));
+    $("#result").children("li").remove();
+    const ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+    const hearts = [];
+    const spades = [];
+    const clubs = [];
+    const diamonds = [];
+    ranks.forEach(element => {
+      hearts.push(element.concat(" of hearts"));
+    });
+    ranks.forEach(element => {
+      spades.push(element.concat(" of spades"));
+    });
+    ranks.forEach(element => {
+      clubs.push(element.concat(" of clubs"));
+    });
+    ranks.forEach(element => {
+      diamonds.push(element.concat(" of diamonds"));
+    });
+    const deck = hearts.concat(diamonds, spades, clubs);
+    deck.forEach(element => {
+      $("ul#result").append("<li>" + element + "</li>")
+    });
+    $("#result").show();
   });
 });
