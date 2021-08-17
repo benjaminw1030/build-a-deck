f06b54417efd48519a97507cacc7a8b5c6886a2b
Ben Wilson <benjaminw1030@gmail.com>
2021-08-16 20:49:37 -0700

    update javascript to add nested loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,13 @@ $(document).ready(function () {
   $("#deckForm").submit(function (event) {
     event.preventDefault();
     $("#result").children("li").remove();
-    const ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
-    const hearts = [];
-    const spades = [];
-    const clubs = [];
-    const diamonds = [];
-    ranks.forEach(element => {
-      hearts.push(element.concat(" of hearts"));
-    });
-    ranks.forEach(element => {
-      spades.push(element.concat(" of spades"));
-    });
-    ranks.forEach(element => {
-      clubs.push(element.concat(" of clubs"));
-    });
-    ranks.forEach(element => {
-      diamonds.push(element.concat(" of diamonds"));
-    });
-    const deck = hearts.concat(diamonds, spades, clubs);
-    deck.forEach(element => {
-      $("ul#result").append("<li>" + element + "</li>")
+    const rank = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+    const suit = ["hearts", "diamonds", "spades", "clubs"];
+
+    suit.forEach(suit => {
+      rank.forEach(rank => {
+        $("ul#result").append("<li>" + rank + " of " + suit + "</li>");
+      });  
     });
     $("#result").show();
   });
